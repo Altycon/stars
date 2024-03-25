1d6eca6de98313da90d313f51189a89de6898331
Altycon <altycon1@gmail.com>
2024-03-24 22:41:33 -0700

    adding game of life animation

--- js/app.js
+++ js/app.js
@@ -1,13 +1,91 @@
-
+import { GameOfLife } from "./game_of_life.js";
 
 const APP = {
 
+    state: 'started',
     DPI: devicePixelRatio,
     canvas: undefined,
     context: undefined,
     canvasWidth: undefined,
     canvasHeight: undefined,
+    animationId: undefined,
+    animationTime: undefined,
+    animationCallback: undefined,
 
+    setState(state){
+
+        APP.state = state;
+
+    },
+    setAnimation(animation){
+
+        switch(animation){
+
+            case 1:
+
+            APP.animationCallback = ()=>{
+
+                GameOfLife.update();
+    
+                GameOfLife.renderGrid();
+            }
+
+            break;
+
+            case 2:
+
+            break;
+        }
+
+    },
+    selectAnimation(event){
+
+        event.preventDefault();
+        
+        document.querySelectorAll('.animation-options button').forEach( animationButton => {
+
+            if(animationButton.classList.contains('active')){
+
+                animationButton.classList.remove('active');
+            }
+
+        });
+
+        APP.setAnimation(event.target.value);
+
+        event.target.classList.add('active');
+
+    },
+    stopAnimation(event){
+
+        event.preventDefault();
+
+        APP.setState('stopped');
+
+        APP.animationId = undefined;
+
+    },
+    startAnimation(event){
+
+        event.preventDefault();
+
+        APP.setState('started');
+
+        APP.animate();
+
+    },
+    listen(){
+
+        document.querySelectorAll('.animation-options button').forEach( animationButton => {
+
+            animationButton.addEventListener('click', APP.selectAnimation);
+
+        });
+
+        document.querySelector('#AnimationStartButton').addEventListener('click', APP.startAnimation);
+        document.querySelector('#AnimationStopButton').addEventListener('click', APP.stopAnimation);
+
+    },
     fixCanvas(canvas){
         const main = document.querySelector('.main-content');
 
@@ -17,14 +95,59 @@ const APP = {
         canvas.setAttribute('height', styleHeight * APP.DPI);
         return canvas;
     },
+    renderText(text,x,y,color){
+
+        APP.context.fillStyle = color;
+        APP.context.font = '30px sans-serif';
+        APP.context.textAlign = 'center';
+        APP.context.fillText(text, x, y);
+    },
+    animate(){
+
+        APP.animationTime = performance.now();
+
+        function loop(timestamp){
+
+            const delta = timestamp - APP.animationTime;
+
+            APP.animationCallback();
+
+            if(APP.state === 'stopped'){
+
+                cancelAnimationFrame(APP.animationId);
+
+            }else{
+
+                APP.animationTime = timestamp;
+
+                requestAnimationFrame(loop);
+            }
+        }
+
+        APP.animationId = requestAnimationFrame(loop);
+
+        console.log(APP.animationId)
+    },
     initialize(){
         
         APP.canvas = APP.fixCanvas(document.querySelector('.canvas'));
 
-        APP.context = APP.canvas.getContext('2d');
+        // APP.context = APP.canvas.getContext('2d');
 
-        APP.canvasWidth = APP.canvas.width;
-        APP.canvasHeight = APP.canvas.height;
+        // APP.canvasWidth = APP.canvas.width;
+        // APP.canvasHeight = APP.canvas.height;
+
+        // APP.renderText('This is new', APP.canvasWidth*0.5, APP.canvasHeight*0.5,'red');
+
+        GameOfLife.initialize(APP.canvas);
+
+        GameOfLife.setColor('hsl(180 100% 50%)');
+
+        GameOfLife.buildRandomGrid();
+
+        APP.listen();
+
+        APP.setAnimation(1);
     }
 };
 
---
+++ js/game_of_life.js
@@ -0,0 +1,233 @@
+
+
+export const GameOfLife = {
+    canvas: undefined,
+    overlayCanvas: undefined,
+    resolution: 5,
+    context: undefined,
+    overlayContext: undefined,
+    fillStyle: 'hsl(180 100% 50%)',
+    width: undefined,
+    height: undefined,
+    columns: undefined,
+    rows: undefined,
+    gridLength: undefined,
+    grid: undefined,
+    nextGeneration: undefined,
+
+    setColor(color){
+
+        GameOfLife.context.fillStyle = color;
+    },
+    loopGrid(callback){
+        for(let i = 0; i < GameOfLife.gridLength; i++){
+            callback(i);
+        }
+    },
+    buildGrid(){
+        GameOfLife.loopGrid((i)=>{
+            GameOfLife.grid[i] = 0;
+        });
+    },
+    buildRandomGrid(){
+        GameOfLife.loopGrid((i)=>{
+            GameOfLife.grid[i] = Math.random() > 0.5 ? 1:0;
+        });
+    },
+    checkNeighbors(row,col){
+        let numberOfNeighbors = 0;
+        
+        for(let i = -1; i <= 1; i++){
+            for(let j = -1; j <= 1; j++){
+
+                const r = (row + i + GameOfLife.rows) % GameOfLife.rows;
+                const c = (col + j + GameOfLife.columns) % GameOfLife.columns;
+                const index = r * GameOfLife.columns + c;
+                numberOfNeighbors += GameOfLife.grid[index];
+
+            }
+        }
+        return numberOfNeighbors - GameOfLife.grid[row * GameOfLife.columns + col];
+    },
+    update(){
+        GameOfLife.loopGrid((i)=>{
+
+            GameOfLife.nextGeneration[i] = GameOfLife.grid[i];
+
+        });
+
+        GameOfLife.loopGrid((i)=>{
+
+            const cell = GameOfLife.grid[i];
+
+            const row = Math.floor(i / GameOfLife.columns);
+            const column = i % GameOfLife.columns;
+
+            const numberOfNeighbors = GameOfLife.checkNeighbors(row,column);
+
+            if((cell & 1) === 1 && (numberOfNeighbors < 2 || numberOfNeighbors > 3)){
+
+                GameOfLife.nextGeneration[i] = 0;
+
+            }else if((cell & 1) === 0 && (numberOfNeighbors & 3) === 3){
+
+                GameOfLife.nextGeneration[i] = 1;
+
+            }
+
+        });
+
+        GameOfLife.loopGrid((i)=>{
+
+            GameOfLife.grid[i] = GameOfLife.nextGeneration[i];
+
+        });
+    },
+    clearCanvas(){
+
+        GameOfLife.context.clearRect(0,0,GameOfLife.width,GameOfLife.height);
+    },
+    renderGrid(){
+        GameOfLife.clearCanvas();
+
+        GameOfLife.loopGrid((i)=>{
+
+            const row = Math.floor(i / GameOfLife.columns);
+            const col = i % GameOfLife.columns;
+            const cell = GameOfLife.grid[i];
+            
+            if(cell === 1){
+
+                GameOfLife.context.fillRect(
+                    col * GameOfLife.resolution, 
+                    row * GameOfLife.resolution, 
+                    GameOfLife.resolution, 
+                    GameOfLife.resolution
+                );
+            }
+
+        });
+        
+    },
+    initialize(canvasElement){
+
+        GameOfLife.canvas = canvasElement;
+        GameOfLife.context = canvasElement.getContext('2d');
+        GameOfLife.width = canvasElement.width;
+        GameOfLife.height = canvasElement.height;
+
+        GameOfLife.columns = Math.floor(GameOfLife.width / GameOfLife.resolution);
+        GameOfLife.rows = Math.floor(GameOfLife.height / GameOfLife.resolution);
+
+        GameOfLife.gridLength = GameOfLife.columns * GameOfLife.rows;
+
+        GameOfLife.grid = new Uint8Array(GameOfLife.gridLength);
+        GameOfLife.nextGeneration = new Uint8Array(GameOfLife.gridLength);
+
+        GameOfLife.context.fillStyle = GameOfLife.fillStyle;
+    }
+}
+
+class GameOfLifeOptimized{
+    constructor(canvas, offscreen, resolution){
+        this.canvas = canvas;
+        this.offscreenCanvas = offscreen;
+        this.resolution = resolution || 40;
+        this.context = this.canvas.getContext('2d');
+        this.offContext = this.offscreenCanvas.getContext('2d');
+        this.offContext.fillStyle = 'hsl(180 100% 50%)';
+        this.width = this.canvas.width;
+        this.height = this.canvas.height;
+        this.columns = Math.floor(this.width/this.resolution);
+        this.rows = Math.floor(this.height/this.resolution);
+        this.gridLength = this.columns*this.rows;
+        this.grid = new Uint8Array(this.gridLength);
+        this.nextGeneration = new Uint8Array(this.gridLength);
+
+        this.canvas.addEventListener('pointerdown', this.handlePointerDown.bind(this));
+    }
+    loopGrid(callback){
+        for(let i = 0; i < this.gridLength; i++){
+            callback(i);
+        }
+    }
+    buildGrid(){
+        this.loopGrid((i)=>{
+            this.grid[i] = 0;
+        });
+    }
+    buildRandomGrid(){
+        this.loopGrid((i)=>{
+            this.grid[i] = Math.random() > 0.5 ? 1:0;
+        });
+    }
+    selectGridCell(ev){
+        const rect = this.canvas.getBoundingClientRect();
+        const x = ev.clientX - rect.left;
+        const y = ev.clientY - rect.top;
+
+        const row = Math.floor( y / this.resolution);
+        const column = Math.floor( x / this.resolution);
+
+        const index = row * this.columns + column;
+        this.grid[index] = !this.grid[index];
+    }
+   
+    handlePointerDown(ev){
+        this.selectGridCell(ev);
+    }
+    checkNeighbors(row,col){
+        let numberOfNeighbors = 0;
+        
+        for(let i = -1; i <= 1; i++){
+            for(let j = -1; j <= 1; j++){
+                const r = (row + i + this.rows) % this.rows;
+                const c = (col + j + this.columns) % this.columns;
+                const index = r * this.columns + c;
+                numberOfNeighbors += this.grid[index];
+            }
+        }
+        return numberOfNeighbors - this.grid[row * this.columns + col];
+    }
+    update(){
+        this.loopGrid((i)=>{
+            this.nextGeneration[i] = this.grid[i];
+        });
+        this.loopGrid((i)=>{
+            const cell = this.grid[i];
+
+            const row = Math.floor(i / this.columns);
+            const column = i % this.columns;
+
+            const numberOfNeighbors = this.checkNeighbors(row,column);
+
+            if((cell & 1) === 1 && (numberOfNeighbors < 2 || numberOfNeighbors > 3)){
+                this.nextGeneration[i] = 0;
+            }else if((cell & 1) === 0 && (numberOfNeighbors & 3) === 3){
+                this.nextGeneration[i] = 1;
+            }
+        });
+        this.loopGrid((i)=>{
+            this.grid[i] = this.nextGeneration[i];
+        });
+    }
+    renderGrid(){
+        this.offContext.clearRect(0,0,this.width,this.height);
+        this.loopGrid((i)=>{
+            const row = Math.floor(i / this.columns);
+            const col = i % this.columns;
+            const cell = this.grid[i];
+            
+            if(cell === 1){
+                this.offContext.fillRect(
+                    col * this.resolution, 
+                    row * this.resolution, 
+                    this.resolution, 
+                    this.resolution
+                );
+            }
+        });
+        this.context.clearRect(0,0,this.width,this.height);
+        this.context.drawImage(this.offscreenCanvas, 0, 0);
+    }
+}
